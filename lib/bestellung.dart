70b96f9488dce4bcf9bda60c62a7228ba4530410
tareknassri <taatnaan@gmail.com>
2023-02-26 18:04:52 +0100

    Changes

--- lib/bestellung.dart
+++ lib/bestellung.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 
 
+//FDKLMSFLNVJFSADVNÄSKJBÖAFNSDVÖJKAC
 class GeeksForGeeks extends StatelessWidget {
   const GeeksForGeeks({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(child: Text('Hello World')),
+      home: Center(child: Text('Hello World ASDFVFDAC')),
     );
   }
 }
